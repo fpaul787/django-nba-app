fda11071a1ac7d1352dc49025622a56bb0d85332
Frantz Paul <fpaul787@gmail.com>
2020-06-26 21:54:34 -0400

    changed alert component to class component

--- src/components/layout/Alert/Alert.js
+++ src/components/layout/Alert/Alert.js
@@ -1,25 +1,42 @@
 import React from 'react'
+import { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-const Alert = ({ alerts }) =>
-    alerts !== null &&
-    alerts.length > 0 &&
-    alerts.map((alert) => (
-        <div
-            key={alert.id}
-            style={{
-                textAlign: 'center',
-                marginTop: '15px',
-                marginLeft: '25%',
-                marginRight: '25%',
-            }}
-            className={`alert alert-${alert.alertType} center-block`}
-            role="alert"
-        >
-            {alert.msg}
-        </div>
-    ))
+class Alert extends Component {
+
+    render() {
+        const { alerts } = this.props
+
+        if (alerts.length < 1) {
+            return (<div></div>)
+        }
+        else {
+            return (
+                <div>
+                    {alerts.map((alert) => {
+                        return (
+                            <div
+                                key={alert.id}
+                                style={{
+                                    textAlign: 'center',
+                                    marginTop: '15px',
+                                    marginLeft: '25%',
+                                    marginRight: '25%',
+                                }}
+                                className={`alert alert-${alert.alertType} center-block`}
+                                role="alert"
+                            >
+                                {alert.msg}
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
+
+    }
+}
 
 Alert.propTypes = {
     alerts: PropTypes.array.isRequired,
